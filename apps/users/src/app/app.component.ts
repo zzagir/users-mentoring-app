73e41d0f8b016cd0e0273801419df6cd10f8029a
Zagir <zaynalov005@mail.ru>
2024-01-29 20:29:11 +0300

    refactor: small refactor code

--- apps/users/src/app/app.component.ts
+++ apps/users/src/app/app.component.ts
@@ -1,18 +1,17 @@
-import { Component, inject } from "@angular/core";
-import { RouterModule } from "@angular/router";
-import { NxWelcomeComponent } from "./nx-welcome.component";
-import { UsersService } from "./users.service";
-import { HttpClientModule } from "@angular/common/http";
+import { Component, inject } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { UsersService } from './users.service';
+import { FooterComponent, HeaderComponent } from '@users/layout';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule, HttpClientModule],
-  selector: "users-root",
-  templateUrl: "./app.component.html",
-  styleUrl: "./app.component.scss",
-  providers: []
+  imports: [RouterModule, HeaderComponent, FooterComponent],
+  selector: 'users-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+  providers: [],
 })
 export class AppComponent {
-  title = "users";
+  title = 'users';
   private readonly userService = inject(UsersService);
 }
--- libs/users/core/ui/layout/src/index.ts
+++ libs/users/core/ui/layout/src/index.ts
@@ -1,1 +1,2 @@
-export * from './lib/layout/layout.component';
+export * from "./lib/header/header.component";
+export * from "./lib/footer/footer.component";
