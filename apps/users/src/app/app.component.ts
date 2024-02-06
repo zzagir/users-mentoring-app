171250a98b9f2eafa529053de8b4ef036a5ab20b
Zagir <zaynalov005@mail.ru>
2024-02-06 19:30:23 +0300

    fix: remove taiga

--- apps/users/src/app/app.component.ts
+++ apps/users/src/app/app.component.ts
@@ -1,16 +1,14 @@
-import { NgDompurifySanitizer } from "@tinkoff/ng-dompurify";
-import { TuiRootModule, TuiDialogModule, TuiAlertModule, TUI_SANITIZER } from "@taiga-ui/core";
 import { Component } from "@angular/core";
 import { RouterModule } from "@angular/router";
 import { FooterComponent, HeaderComponent } from "@users/layout";
 
 @Component({
   standalone: true,
-  imports: [RouterModule, HeaderComponent, FooterComponent, TuiRootModule, TuiDialogModule, TuiAlertModule],
+  imports: [RouterModule, HeaderComponent, FooterComponent],
   selector: "users-root",
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.scss",
-  providers: [{provide: TUI_SANITIZER, useClass: NgDompurifySanitizer}]
+  providers: []
 })
 export class AppComponent {
   title = "users";
--- apps/users/src/app/app.config.ts
+++ apps/users/src/app/app.config.ts
@@ -1,5 +1,4 @@
 import { provideAnimations } from "@angular/platform-browser/animations";
-import { TuiRootModule } from "@taiga-ui/core";
 import { ApplicationConfig, importProvidersFrom } from "@angular/core";
 import { provideRouter } from "@angular/router";
 import { appRoutes } from "./app.routes";
@@ -10,23 +9,23 @@ import { provideEffects } from "@ngrx/effects";
 import { provideState, provideStore } from "@ngrx/store";
 import { USERS_FEATURE_KEY, UsersFacade, usersReducer, userEffects } from "@users/users/users/data-access";
 import { provideStoreDevtools } from "@ngrx/store-devtools";
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideAnimationsAsync } from "@angular/platform-browser/animations/async";
 
 
 export const appConfig: ApplicationConfig = {
   providers: [
-        provideAnimations(),
-        provideEffects(userEffects),
+    provideAnimations(),
+    provideEffects(userEffects),
     provideState(USERS_FEATURE_KEY, usersReducer),
     provideStore(),
     UsersFacade,
     provideStoreDevtools(),
     provideRouter(appRoutes),
     provideHttpClient(),
-    importProvidersFrom(TuiRootModule),
+    importProvidersFrom(),
     {
       provide: API_URL,
       useValue: environment.API_URL
-    }, provideAnimationsAsync('noop')
+    }, provideAnimationsAsync("noop")
   ]
 };
